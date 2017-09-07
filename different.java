af8a306a556214949833b502cee9e822b467c5d6
julianstauta <julianstauta@gmail.com>
2017-09-07 12:09:58 -0500

    Update and rename Different.java to different.java

--- different.java
+++ different.java
@@ -5,7 +5,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
-public class Different {
+public class different {
 
 	public static void main(String[] args) throws IOException{
 		
