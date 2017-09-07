975f548e837253f95aa2ac13b68738939979d6b0
julianstauta <julianstauta@gmail.com>
2017-09-07 12:16:17 -0500

    Update and rename SuperComputer.java to supercomputer.java

--- supercomputer.java
+++ supercomputer.java
@@ -5,7 +5,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
-public class SuperComputer {
+public class supercomputer {
 	
 	public static void main(String[] args) throws IOException{
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
