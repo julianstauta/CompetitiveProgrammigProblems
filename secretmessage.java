a50e02b8a117e8cb93331efb90145a3768c90a14
julianstauta <julianstauta@gmail.com>
2017-09-07 12:15:30 -0500

    Update and rename SecretMessage.java to secretmessage.java

--- secretmessage.java
+++ secretmessage.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
-public class SecretMessage {
+public class secretmessage {
 
 	public static void main(String[] args) throws IOException {
 
