c587e930198ea0dbb40eada65c2a6fa07e21df9e
julianstauta <julianstauta@gmail.com>
2017-09-07 12:04:21 -0500

    Update and rename Autori.java to autori.java

--- autori.java
+++ autori.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
-public class Autori {
+public class autori {
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
